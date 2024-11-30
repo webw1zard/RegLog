200e456c543bb7577a96dcc9aa326d861c644a9e
webw1zard <ulugbeknizomov999@gmail.com>
2024-11-30 16:43:51 +0500

    loginpathname

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Login';      
 import Register from './Register'; 
 import Cabinet from './Cabinet';  
 
 const App: React.FC = () => {
+  const blockPage = ["/cabinet"]
+  const navigate = useNavigate()
+  useEffect(()=>{
+    const id = localStorage.getItem('id')
+    if(!id){
+      if(blockPage.includes(location.pathname)){
+        navigate('/login')
+      }
+    }
+  })
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
